83f2bfb0e4486dca7f37eaf44e6ac5cc5cc2a488
ismoil793 <ismoil.793@gmail.com>
2020-06-12 16:10:41 +0500

    add build

---
+++ client/build/precache-manifest.d05dc3842ac17e6b0b3361e61d6853ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931d0460c03c1725d7f6940ac3ee03eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e3d0fbfd38a8783fc6",
+    "url": "/static/css/main.bb4a9f6a.chunk.css"
+  },
+  {
+    "revision": "2f2aaa7e18463c06157e",
+    "url": "/static/js/2.2a8a645d.chunk.js"
+  },
+  {
+    "revision": "910ce3944b6d5336eca6f60b8c3298cd",
+    "url": "/static/js/2.2a8a645d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e3d0fbfd38a8783fc6",
+    "url": "/static/js/main.3f4c4bf0.chunk.js"
+  },
+  {
+    "revision": "c69801d17d163cd039ca",
+    "url": "/static/js/runtime-main.987a4d46.js"
+  }
+]);
