5348add206f57aec854559c3dd62f23e0045b258
ismoil793 <ismoil.793@gmail.com>
2020-06-12 16:12:17 +0500

    add build

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(cookiesParser());
 app.use(fileUpload());
 
-// app.use(express.static("client/build"));  // needed for production
+// app.use(express.static("client/build"));  // needed for: production
 
 
 //-------------------------------------------------------------------------- FILES //
